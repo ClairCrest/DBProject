d7ca8e9a28dc6df37447a845168fe4025b6f5bd5
Karbys <Bewantza33@gmail.com>
2024-05-09 23:12:32 +0700

    Update index.php

--- transfer/index.php
+++ transfer/index.php
@@ -6,11 +6,6 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="/assets/css/style.css" rel="stylesheet">
-    <style>
-        body {
-            background-color: #f0f0f0; /* Light grey color */
-        }
-    </style>
 </head>
 <body>
 
