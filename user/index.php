c1779b258145861b62d69f749ad20d8d90357e07
Karbys <Bewantza33@gmail.com>
2024-05-09 21:05:29 +0700

    Update index.php

--- user/index.php
+++ user/index.php
@@ -1,5 +1,6 @@
 <?php 
     session_start();
+    require_once  '../config/db.php';
     if(!isset($_SESSION['user_login']))
     {
         $_SESSION['error'] = 'กรุณาเข้าสู่ระบบ';
@@ -11,9 +12,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="/assets/css/style.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Welcome USER</h1>
+    <div class="container">
+        <?php 
+            if(isset($_SESSION['user_login']))
+            {
+                //เอาไว้ query data หาข้อมูล
+                $user_id = $_SESSION['user_login'];
+                $stmt = $conn->query("SELECT * FROM users WHERE id = $user_id");
+                $stmt-> execute();
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+        ?>
+        <h1>Welcome <?php echo $row['firstname']. ' ' .$row['lastname']?> User</h1>
+        <a href="../logout/" class="btn btn-danger">Log out</a>
+    </div>
 </body>
 </html>
