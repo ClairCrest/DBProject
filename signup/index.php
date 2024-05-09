f555418b3f6164a1276dc189a73ea1de4bf244eb
Karbys <Bewantza33@gmail.com>
2024-05-09 14:02:43 +0700

    Update index.php

--- signup/index.php
+++ signup/index.php
@@ -10,48 +10,48 @@
   <body>
   <div class="container mt-3">
     <h1>KMUTT666 Registration</h1>
+    <hr>
     <form> <div class="row">
-        <div class="col-md-6 mb-3">
+        <div class="col-md-6 mt-3">
           <label for="firstname" class="form-label">Name</label>
           <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Enter Firstname">
         </div>
-        <div class="col-md-6 mb-3">
+        <div class="col-md-6 mt-3">
           <label for="lastname" class="form-label">Lastname</label>
           <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Enter Lastname">
         </div>
       </div>
       <div class="row">
-        <div class="col-md-6 mb-3">
+        <div class="col-md-6 mt-3">
           <label for="citizenID" class="form-label">CitizenID</label>
           <input type="text" class="form-control" id="citizenID" name="citizenID" placeholder="Enter Citizen ID">
         </div>
-        <div class="col-md-6 mb-3">
+        <div class="col-md-6 mt-3">
           <label for="telephone" class="form-label">Telephone</label>
           <input type="text" class="form-control" id="telephone" name="telephone" placeholder="Enter Telephone">
         </div>
       </div>
       <div class="row">
-        <div class="col-md-6 mb-3">
+        <div class="col-md-6 mt-3">
           <label for="email" class="form-label">Email</label>
           <input type="text" class="form-control" id="email" name="email" placeholder="Enter Email">
         </div>
-        <div class="col-md-6 mb-3">
+        <div class="col-md-6 mt-3">
           <label for="province" class="form-label">Province</label>
           <input type="text" class="form-control" id="province" name="province" placeholder="Enter Province">
         </div>
       </div>
       <div class="row">
-        <div class="col-md-6 mb-3">
+        <div class="col-md-6 mt-3">
           <label for="password" class="form-label">Password</label>
           <input type="text" class="form-control" id="password" name="password" placeholder="Enter Password">
         </div>
-        <div class="col-md-6 mb-3">
+        <div class="col-md-6 mt-3">
           <label for="confirm_password" class="form-label">Confirm Password</label>
           <input type="text" class="form-control" id="confirm_password" name="confirm_password" placeholder="Confirm Password">
         </div>
       </div>
-      
-      <button type="submit" class="btn btn-primary position-absolute bottom-0 start-50 translate-middle-x"><a href="/Homepage/">Register</a></button>
+      <a class="btn btn-primary w-100 mt-5" href="../index.php" role="button">Register</a>
 
     </form>
     <a href="/Homepage/">
