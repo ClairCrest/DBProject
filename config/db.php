651bea9f8957887587daff5849f887456459624f
Karbys <Bewantza33@gmail.com>
2024-05-10 00:10:48 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=registeration_system", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=transaction", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   //echo "Connected successfully";
