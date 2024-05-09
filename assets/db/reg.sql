ec3208bec99944cbd9e591252470e5fa4cc2f089
Karbys <Bewantza33@gmail.com>
2024-05-09 15:59:36 +0700

    Update reg.sql

--- assets/db/reg.sql
+++ assets/db/reg.sql
@@ -1,12 +1,12 @@
-CREATE TABLE user (
+CREATE TABLE users (
     id int(11) NOT NULL  AUTO_INCREMENT PRIMARY KEY,
     firstname varchar(255) NOT NULL,
     lastname varchar(255) NOT NULL,
-    citiizen_ID int(15) NOT NULL,
-    telephone int(11) NOT NULL,
+    citizen_ID varchar(255) NOT NULL,
+    telephone varchar(255) NOT NULL,
     email varchar(255) NOT NULL,
     province varchar(255) NOT NULL, 
-    password varchar(100) NOT NULL,
+    password varchar(255) NOT NULL,
     urole varchar(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
