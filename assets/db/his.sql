3b2058407b355e965a2cf09f199cd016e313d243
Karbys <Bewantza33@gmail.com>
2024-05-10 21:36:08 +0700

    Update his.sql

--- assets/db/his.sql
+++ assets/db/his.sql
@@ -1,8 +1,8 @@
 CREATE TABLE history (
     id int(11) NOT NULL  AUTO_INCREMENT PRIMARY KEY,
     target_id int(11), 
-    old_balance int(11) NOT NULL,
-    new_balance int(11) NOT NULL,
-    difference int(11) NOT NULL,
+    old_balance float(20) NOT NULL,
+    new_balance float(20) NOT NULL,
+    difference float(20) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
