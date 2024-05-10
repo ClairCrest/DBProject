1697af251c674e935024adb80a12512ffd3e8b79
Karbys <Bewantza33@gmail.com>
2024-05-10 20:38:15 +0700

    Update deposit.php

--- deposit/deposit.php
+++ deposit/deposit.php
@@ -4,6 +4,16 @@ require_once '../config/db.php';
 
 if(isset($_POST['deposit'])) 
 {
-    $money = $_POST['money']
+    $money = $_POST['money'];
+    if(empty($money))
+    {
+        $_SESSION['error'] = "กรุณากรอกจำนวนเงิน";
+        header("location: ../deposit/index.php");
+    }
+    else if (!filter_var($money, FILTER_VALIDATE_FLOAT)) 
+    {
+        $_SESSION['error'] = 'กรุณากรอกใหม่';
+        header("location: ../deposit/index.php");
+    }
 } 
 ?>
