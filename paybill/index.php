ccc3cac8b6afc32d83b77e8252412d6a675370e7
Karbys <Bewantza33@gmail.com>
2024-05-10 22:27:47 +0700

    update paybill

--- paybill/index.php
+++ paybill/index.php
@@ -1,3 +1,13 @@
+<?php 
+  session_start();
+  require_once '../config/db.php';
+  if(!isset($_SESSION['user_login']))
+  {
+    $_SESSION['error'] = 'กรุณาเข้าสู่ระบบ';
+    header("location: ../index.php");
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,42 +16,43 @@
   <title>Pay bill</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link href="/assets/css/style.css" rel="stylesheet">
-  <style>
-    .card-img-top {
-        height: 10px; /* Set a fixed height for the card image */
-        max-height: 200px;
-        object-fit: cover; /* Ensure the image covers the entire space */
-    }
-  </style>
-
 </head>
 <body>
-  <div class="position-absolute top-0 start-50 translate-middle-x mt-3">
-    <h1 class="ms-4">Pay Bills</h1>
-    <div class="my-5"><img src="/assets/img/paybill1.png" alt="" class="logo"></div>
-  </div>
-
-  <div class="row position-absolute top-50 start-50 translate-middle ms-4 mt-5">
-    <div class="col-md-5">
-      <div class="card flex-grow-1 me-2 h-100"> <!-- Added h-100 class for fixed height -->
-        <img src="/assets/img/elec1.png" class="card-img-top w-100 h-100" alt="..."> <!-- Added w-100 and h-100 classes for fixed width and height -->
-        <div class="card-body">
-          <h5 class="card-title">Electricity</h5>
-          <p class="card-text">Pay Electricity</p>
-          <a href="#" class="btn btn-primary">Pay</a>
-        </div>
+  <div class="position-absolute top-0 start-50 translate-middle-x mt-3 text-white w-25">
+    <form action="../paybill/payment.php" method="post">
+            <?php if (isset($_SESSION['error'])) { ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php
+                            echo$_SESSION['error'];
+                            unset($_SESSION['error']);
+                        ?>
+                    </div>
+                <?php } ?>
+                <?php if (isset($_SESSION['success'])) { ?>
+                    <div class="alert alert-success" role="alert">
+                        <?php
+                            echo$_SESSION['success'];
+                            unset($_SESSION['success']);
+                        ?>
+                    </div>
+            <?php } ?>
+    <fieldset usabled>
+      <legend>Pay bill</legend>
+      <div class="mb-3">
+        <label for="disabledTextInput" class="form-label">Payment</label>
+        <input type="text" class="form-control" name="money" placeholder="Amount">
       </div>
-    </div>
-    <div class="col-md-5">
-      <div class="card flex-grow-1 h-100"> <!-- Added h-100 class for fixed height -->
-        <img src="/assets/img/water1.png" class="card-img-top w-100 h-100" alt="..."> <!-- Added w-100 and h-100 classes for fixed width and height -->
-        <div class="card-body">
-          <h5 class="card-title">Water</h5>
-          <p class="card-text">Pay Water</p>
-          <a href="#" class="btn btn-primary">Pay</a>
-        </div>
+      <div class="mb-3">
+        <label for="disabledSelect" class="form-label">Select Option</label>
+        <select id="disabledSelect" class="form-select">
+          <option>Electricity</option>
+          <option>Water</option>
+        </select>
       </div>
-    </div>
+      <a href="../user/" class="btn btn-danger">Back</a>
+      <button type="submit" class="btn btn-primary" name="pay">Submit</button>
+    </fieldset>
+    </form>
   </div>
 </body>
 </html>
