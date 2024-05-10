bcac69f026231a3109b4a74f9bc99a5e395457e6
ClairCrest <topwarapol8850@gmail.com>
2024-05-10 21:26:48 +0700

    Update his.sql
    Word Correction and add target_id collumn.

--- assets/db/his.sql
+++ assets/db/his.sql
@@ -1,7 +1,8 @@
 CREATE TABLE history (
     id int(11) NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+    target_id int(11), 
     old_balance int(11) NOT NULL,
     new_balance int(11) NOT NULL,
-    different int(11) NOT NULL,
+    difference int(11) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
