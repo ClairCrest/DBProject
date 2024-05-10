9f874479560c5e6b0b63ff2b3a261794fc8a00b2
Karbys <Bewantza33@gmail.com>
2024-05-10 21:52:49 +0700

    Update index.php

--- withdraw/index.php
+++ withdraw/index.php
@@ -1,43 +1,50 @@
+<?php
+    session_start();
+    require_once '../config/db.php';
+    if(!isset($_SESSION['user_login']))
+    {
+        $_SESSION['error'] = 'กรุณาเข้าสู่ระบบ';
+        header("location: ../index.php");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Withdraw</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="/assets/css/style.css" rel="stylesheet">
 </head>
 <body>
-<div class="container d-flex justify-content-center align-items-center vh-100"> <div class="position-absolute top-0 start-50 translate-middle-x">
-    <div class="text-center">
-        <div class="mt-3">
-                <h2 class="mb-1 mx-3">Account</h2>
-                <h3 class="mt-0">012-3-***456</h3>
-                <h1 class="mx-2 mb-5">999 THB</h1>
-            </div>
-        </div>
-
-        <div class="my-5">
-            <h2 class="text-center">Withdraw</h2>
-            <div class="row">
-                <div class="col-md-auto">
-
-            <div class="form-group ">
-                <input type="text" class="form-control" name="amount" placeholder="Enter amount of your withdrawal">
-            </div>
-
-            <div class="row mt-3">
-                <div class="col-md-5">
-                    <a class="btn btn-danger btn-primary w-100 mx-0" href="/Homepage/" role="button">Back</a>
+    <div class="position-absolute top-50 start-50 text-center translate-middle">
+        <form action="../withdraw/withdraw.php" method="post">
+            <?php if (isset($_SESSION['error'])) { ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php
+                            echo$_SESSION['error'];
+                            unset($_SESSION['error']);
+                        ?>
+                    </div>
+                <?php } ?>
+                <?php if (isset($_SESSION['success'])) { ?>
+                    <div class="alert alert-success" role="alert">
+                        <?php
+                            echo$_SESSION['success'];
+                            unset($_SESSION['success']);
+                        ?>
+                    </div>
+            <?php } ?>
+            <fieldset usabled class="text-white">
+                <legend>Withdraw</legend>
+                <div class="mb-3">
+                    <input type="text" class="form-control text-center" name="money" placeholder="Add Money">
                 </div>
-                <div class="col-md-5">
-                    <a class="btn btn-success btn-primary w-100 mx-5" href="#" role="button">Confirm</a>
-                </div>
-            </div>
-
-        </div>    
+                <a href="../user/" class="btn btn-danger">Back</a>
+                <button type="submit" class="btn btn-primary" name="withdraw">Withdraw</button>
+            </fieldset>
+        </form>
     </div>
-</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
